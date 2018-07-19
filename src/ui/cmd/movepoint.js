9bae751e582198c202dd7316397cf8194781df7a
DESKTOP-JU370T5\china <wuguangyue@nn-home.com>
2018-07-19 20:28:31 +0800

    直线的拖动

--- src/ui/cmd/movepoint.js
+++ src/ui/cmd/movepoint.js
@@ -51,7 +51,10 @@ Object.assign(CmdSketcherMovePoint.prototype, {
                         }
                     }
                 } else if (field == "begin" || field == "end") {
+                    if (curve.type == "CurveLine") {
 
+                        curve[field] = pos;
+                    }
                 }
             }
         }
